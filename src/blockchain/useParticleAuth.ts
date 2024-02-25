a5436870b042c2a164162ee84b9b8768ea6151a0
julesl23 <juleslai1234@gmail.com>
2024-02-25 16:42:43 +0000

    Update environment variables and useParticleAuth to include chainName and chainId

--- src/blockchain/useParticleAuth.ts
+++ src/blockchain/useParticleAuth.ts
@@ -31,6 +31,8 @@ export default function useParticleAuth() {
     projectId: process.env.NEXT_PUBLIC_PARTICLE_PROJECT_ID as string,
     clientKey: process.env.NEXT_PUBLIC_PARTICLE_CLIENT_KEY as string,
     appId: process.env.NEXT_PUBLIC_PARTICLE_APP_ID as string,
+    chainName: process.env.NEXT_PUBLIC_CHAIN_NAME as string,
+    chainId: Number(process.env.NEXT_PUBLIC_CHAIN_ID),
     wallet: {
       displayWalletEntry: true,
       defaultWalletEntryPosition: ParticleAuthModule.WalletEntryPosition.BR,
